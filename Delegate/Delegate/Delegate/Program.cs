58a2f68cfbbb1e9822d922f7080e67fd9243c962
truonglx <truonglx@gmail.com>
2021-12-03 17:43:11 +0700

    delegate

---
+++ Delegate/Delegate/Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var topic = new Topic("Topic test")
+            {
+                Serializer = JsonConvert.SerializeObject
+            };
+            topic.OnReceiveMessage += (tp, msg) => { Console.WriteLine($"{topic.Name}:{msg}"); };
+            topic.SendMessage(new
+            {
+                Name = "truonglx",
+                DateTime = DateTime.Now
+            });
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Delegate/Delegate/Delegate/Topic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Delegate
+{
+    public class Topic
+    {
+        public delegate void ReceiveMessage(Topic topic, string message);
+
+        public delegate string Serialize(object message);
+
+        public Serialize Serializer { get; set; }
+
+        public event ReceiveMessage OnReceiveMessage;
+        public string Name { get; }
+
+        public Topic(string name)
+        {
+            Name = name;
+        }
+
+        public void SendMessage(object message)
+        {
+            OnReceiveMessage?.Invoke(this, Serializer == null ? message.ToString() : Serializer.Invoke(message));
+        }
+    }
+}
